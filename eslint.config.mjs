317e850b8676532a497c32c713476d74902a4707
Ritesh Patel <riteshpatel1882004@gmail.com>
2026-05-12 08:44:49 +0530

    added

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,14 +3,18 @@ import nextVitals from "eslint-config-next/core-web-vitals";
 
 const eslintConfig = defineConfig([
   ...nextVitals,
-  // Override default ignores of eslint-config-next.
   globalIgnores([
-    // Default ignores of eslint-config-next:
     ".next/**",
     "out/**",
     "build/**",
     "next-env.d.ts",
   ]),
+  {
+    rules: {
+      "react/no-unescaped-entities": "off",
+      "react-hooks/set-state-in-effect": "off",
+    },
+  },
 ]);
 
 export default eslintConfig;
